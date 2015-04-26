e2c15efff6f5afcf45c4dfcddaa85a04ae92154e
arjunbiddanda <aab227@cornell.edu>
2015-04-26 18:04:16 -0400

    actually added testing mechanism-we should thin down the training dataset

--- testing/testing.cc
+++ testing/testing.cc
@@ -2,15 +2,22 @@
 #include <stdio.h>
 #include <string>
 
-
+#include "../marker.h"
 
 int main(int argc, char **argv){
 	if (argc != 2){
-		fprintf(stderr, "Usage: %s [bim/map file]");
+		fprintf(stderr, "Usage: %s [bim/map file]\n", argv[0]);
 		return 1;
 	}
 
 	// Call some of the genetio methods in here....
+
+
+	//1. Read in a .bim file (> 2 million SNPs)
+	char * chr;
+	strcpy(chr, "1");
+	Marker::readBIMFile(argv[1], chr, 0, 2000000);
+
 
 
 
