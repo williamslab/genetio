c12cb01005b41ce6cc85d0ce8a0085ecc232e4fe
Amy Williams <alw289@cornell.edu>
2017-03-07 11:49:19 -0500

    popcount function

--- util.h
+++ util.h
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <sys/time.h>
 #include <random>
+#include <bitset>
 
 #ifndef UTIL_H
 #define UTIL_H
@@ -139,6 +140,14 @@ inline void mult_printf(FILE *outs[2], const char *msg) {
       continue;
     fprintf(out, "%s", msg);
   }
+}
+
+// Returns the number of bits that have a value of 1
+inline size_t popcount(uint64_t val) {
+  // Currently using std::bitset, but the conversion is probably not free, so
+  // optimize? TODO
+  std::bitset<64> to_count(val);
+  return to_count.count();
 }
 
 #endif // UTIL_H
